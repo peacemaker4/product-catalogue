db35b92c9b615765913dda16d9af95c24f4c6b5f
peacemaker4 <78819130+peacemaker4@users.noreply.github.com>
2025-11-19 01:40:41 +0500

    refactor: make products filter consistent

--- lib/src/features/products/presentation/bloc/products/products_bloc.dart
+++ lib/src/features/products/presentation/bloc/products/products_bloc.dart
@@ -44,6 +44,7 @@ class ProductsBloc extends Bloc<ProductsEvent, ProductsState>{
         products: currentState.products,
         searchQuery: event.query,
         category: currentState.selectedCategory,
+        sortOrder: currentState.sortOrder,
       );
 
       emit(currentState.copyWith(searchQuery: event.query, filteredProducts: filteredProducts));
@@ -62,6 +63,7 @@ class ProductsBloc extends Bloc<ProductsEvent, ProductsState>{
         products: currentState.products,
         searchQuery: currentState.searchQuery,
         category: event.category,
+        sortOrder: currentState.sortOrder,
       );
 
       emit(currentState.copyWith(selectedCategory: event.category, filteredProducts: filteredProducts));
@@ -71,21 +73,16 @@ class ProductsBloc extends Bloc<ProductsEvent, ProductsState>{
   Future<void> _onSortProducts(SortProductsEvent event, Emitter<ProductsState> emit) async {
     if (state is ProductsLoaded) {
       final currentState = state as ProductsLoaded;
-      final productsSorted = currentState.filteredProducts ?? currentState.products;
 
-      switch (event.sortOrder) {
-        case "asc":
-          productsSorted.sort((a, b) => a.price.compareTo(b.price));
-          break;
-        case "desc":
-          productsSorted.sort((a, b) => b.price.compareTo(a.price));
-          break;
-        default:
-          productsSorted.sort((a, b) => a.id.compareTo(b.id)); // исходный порядок по id
-      }
+      final filteredProducts = _applyFilters(
+        products: currentState.products,
+        searchQuery: currentState.searchQuery,
+        category: currentState.selectedCategory,
+        sortOrder: event.sortOrder,
+      );
 
       emit(currentState.copyWith(
-        filteredProducts: List.from(productsSorted),
+        filteredProducts: filteredProducts,
         sortOrder: event.sortOrder,
       ));
     }
@@ -95,11 +92,31 @@ class ProductsBloc extends Bloc<ProductsEvent, ProductsState>{
     required List<Product> products,
     required String searchQuery,
     String? category,
-  }) {
-    return products.where((p) {
+    String? sortOrder,
+  }){
+    final filteredProducts = products.where((p) {
       final matchesCategory = category == null || category.isEmpty || p.category == category;
       final matchesSearch = p.title.toLowerCase().contains(searchQuery.toLowerCase());
       return matchesCategory && matchesSearch;
     }).toList();
+
+    _sortList(products: filteredProducts, sortOrder: sortOrder);
+
+    return filteredProducts;
+  }
+
+  void _sortList({
+    required List<Product> products, String? sortOrder
+  }){
+    switch (sortOrder) {
+      case "asc":
+        products.sort((a, b) => a.price.compareTo(b.price));
+        break;
+      case "desc":
+        products.sort((a, b) => b.price.compareTo(a.price));
+        break;
+      default:
+        products.sort((a, b) => a.id.compareTo(b.id));
+    }
   }
 }
